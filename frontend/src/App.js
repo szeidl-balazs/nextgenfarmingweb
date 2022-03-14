51f1233bab6f3e0ec2c0d0ce281c6e94b9418cf1
bszeidl <szeidl.balazs@hotmail.com>
2022-03-14 08:38:19 +0100

    New css files created till benefits.css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,12 @@
-/*import logo from './logo.svg';*/
+import React from 'react';
 import GridContainer from './components/structure_components/GridContainer';
-import './App.css';
+import './components/css_files/1_base_style.css';
+import './components/css_files/2_1_navigation.css';
+import './components/css_files/2_2_menu_icon.css';
+import './components/css_files/2_3_logo.css';
+import './components/css_files/3_0_main_page.css';
+import './components/css_files/3_1_hero.css';
+import './components/css_files/3_2_benefit.css';
 
 function App() {
   return (
