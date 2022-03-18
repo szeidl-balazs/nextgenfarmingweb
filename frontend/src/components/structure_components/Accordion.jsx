dda467b3afb65e52e8e560b6bd0f12db0d37aa90
bszeidl <szeidl.balazs@hotmail.com>
2022-03-18 16:20:29 +0100

    The accordion open/close works after writing the ternary operator for the component.

--- frontend/src/components/structure_components/Accordion.jsx
+++ frontend/src/components/structure_components/Accordion.jsx
@@ -1,11 +1,14 @@
 import { React, useState } from "react";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
-function Accordion(props) {
+const Accordion = (props) => {
+  
+   //toggle accordion function
   const [open, setOpen] = useState(false);
-  //toggle accordion function
-  let toggleHandler = (e) => {
-    //switch state
+ 
+  
+  //switch state
+  const toggleHandler = (e) => {    
     setOpen(!open);
   };
 
@@ -19,28 +22,40 @@ function Accordion(props) {
           <span className="more-less-sign"><ExpandMoreIcon/></span>
       </button>
 
-      <div className="panel">        
+      {
+        
+        open === true
+        
+        ?
 
-        <div className="panel-wrap">
+        <div className="panel">        
 
-          <div className="panel-text">
-            <p className="accordion__content">{props.text1}</p>
-            <p className="accordion__content">{props.text2}</p>
-            <p className="accordion__content">{props.text3}</p>
-            <p className="accordion__content">{props.text4}</p>
-            <p className="accordion__content">{props.text5}</p>
-            <p className="accordion__content">{props.text6}</p>
-            <p className="accordion__content">{props.text7}</p>
-            <p className="accordion__content">{props.text8}</p>
-          </div>
+          <div className="panel-wrap">
 
-          <div className="specification-img-wrap">
-            <div className="specification-photo" id={props.img_id}></div>
+            <div className="panel-text">
+              <p className="accordion__content">{props.text1}</p>
+              <p className="accordion__content">{props.text2}</p>
+              <p className="accordion__content">{props.text3}</p>
+              <p className="accordion__content">{props.text4}</p>
+              <p className="accordion__content">{props.text5}</p>
+              <p className="accordion__content">{props.text6}</p>
+              <p className="accordion__content">{props.text7}</p>
+              <p className="accordion__content">{props.text8}</p>
+            </div>
+
+            <div className="specification-img-wrap">
+              <div className="specification-photo" id={props.img_id}></div>
+            </div>
+
           </div>
 
         </div>
 
-      </div>
+        : 
+
+        null
+
+      }
 
     </div>
 
