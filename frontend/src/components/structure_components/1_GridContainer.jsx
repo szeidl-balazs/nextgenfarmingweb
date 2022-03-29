14276a091cc0ea45c7d2c934f8a305f77f7ae301
bszeidl <szeidl.balazs@hotmail.com>
2022-03-29 22:27:31 +0200

    Language button ready, but the first try to switch language content failed.

--- frontend/src/components/structure_components/1_GridContainer.jsx
+++ frontend/src/components/structure_components/1_GridContainer.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import {React, useState} from 'react';
 import Header from './3_Header';
 import MainPage from './2_MainPage';
 import Footer from './7_Footer';
@@ -7,13 +7,48 @@ import CookieBar from './9_CookieBar';
 
 const GridContainer = () => {
     
+    const [hungarian, setHungarian] = useState(true);
+
+    const [english, setEnglish] = useState(false);
+
+    const hunSelector = (e) => {
+        setHungarian(true) && setEnglish(false);
+    }
+
+    const engSelector = (e) => {
+        setEnglish(true) && setHungarian(false)
+    }
+
     return (
         <div className="grid-container">
-            <Header />
-            <MainPage />
-            <Footer />
-            <input className="checkbox-cb" id="checkbox-cb" type="checkbox" />
-            <CookieBar />
+           
+            <select className="language-selector">
+                <option selected="selected" value="hun" onClick={hunSelector}>HUN</option>
+                <option value="eng" onClick={engSelector}>ENG</option>
+            </select>
+
+
+        {
+
+            hungarian === true
+
+            ?
+            
+            <div>
+
+                <Header />
+                <MainPage />
+                <Footer />
+                <input className="checkbox-cb" id="checkbox-cb" type="checkbox" />
+                <CookieBar />
+
+            </div>
+            
+            :
+
+            null
+
+        }
             
         </div>
         
