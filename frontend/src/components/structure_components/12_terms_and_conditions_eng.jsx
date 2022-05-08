9280a5b1fb8ef0104031499abd3434877d8bd717
bszeidl <szeidl.balazs@hotmail.com>
2022-05-08 21:32:49 +0200

    The GTC is ready till forms of paxment.

--- frontend/src/components/structure_components/12_terms_and_conditions_eng.jsx
+++ frontend/src/components/structure_components/12_terms_and_conditions_eng.jsx
@@ -10,7 +10,9 @@ const TermsEng = () => {
             <h2>General Terms and Conditions</h2>
 
             <div className="long-text">
-                <p>This General Terms and Conditions (in the followings: GTC) regards to Farkas János sole proprietorship (in the followings: Supplier) and to the client (in the followings: Client) who uses the <span><a href="index.html" target="_blank">www.farkasgym.hu</a></span> web site and the commercial services of the website operated by the Supplier and contains the rights and duties of the parties (the Supplier and the Client in the followings together: Parties). The GTC regards to all such privity and services that occurs through or based on the <span><a href="index.html" target="_blank">www.farkasgym.hu</a></span> website, regardless the place of accomplishment - Hungary or abroad - and regardless that has been performed directly by the Supplier or by its agent. <span class="important">DATA OF SUPPLIER:</span></p>
+
+                <h3>1.- IDENTIFICATION AND BUSINESS NAME OF THE OFFEROR COMPANY.</h3>
+                <p>This General Terms and Conditions (in the followings: GTC) regards to Farkas János sole proprietorship (in the followings: Offeror) and to the user (in the followings: User) who uses the <span><a href="index.html" target="_blank">www.farkasgym.hu</a></span> web site and the commercial services of the website operated by the Offeror and contains the rights and duties of the parties (the Offeror and the User in the followings together: Parties). The GTC regards to all such privity and services that occurs through or based on the <span><a href="index.html" target="_blank">www.farkasgym.hu</a></span> website, regardless the place of accomplishment - Hungary or abroad - and regardless that has been performed directly by the Offeror or by its agent. <span class="important">DATA OF Offeror:</span></p>
                 <p><span class="important">Name: </span>Farkas János sole proprietorship</p>
                 <p><span class="important">Seat and mailing address: </span>Győr, Külső veszprémi út 14 B ép. 9028</p>
                 <p><span class="important">Customer service: </span>see in details in 1.7</p>
@@ -19,6 +21,33 @@ const TermsEng = () => {
                 <p><span class="important">Court registration number: </span>7113188</p>
                 <p><span class="important">Tax number: </span>72395990-1-28</p>
                 <p><span class="important">Bank and bank account number: </span>OTP Győr-Moson-Sopron megyei LAFO fiók (9022 Győr, Teleki út 51.) 11773377-02153434-00000000</p>
+                <p>The address for the purposes of complaints will correspond to that indicated as the company’s registered office.</p>
+                <p>Contracting through the website/online shop of the Offeror shall imply the acceptance by the User or User of the same, as well as the conditions of use established for the User’s navigation through the websites owned by the Offeror. These conditions shall apply without prejudice to the application of the legal regulations on the subject that may be applicable in each case.</p>
+                <p>The Offeror reserves the right to make any changes it deems appropriate, without prior notice, to the GTC. Such modifications may be made, through its website/online shop, by any legally admissible means and shall be binding during the time they are published on the website and until they are validly modified by subsequent ones. However, the Offeror reserves the right to apply, in certain cases, Special Contract Conditions in preference to these GTC when it considers it appropriate, announcing them in due time and form.</p>
+            
+                <h3>2.- CONDITIONS OF USE</h3>
+                <p>The use of the website grants the use of the user and implies the acceptance of the conditions of the general conditions, privacy policy and conditions of use. If the user does not agree, he will refrain from using the website. The User is obliged and commits to use the website and its contents in accordance with the legislation in force, the general conditions and any other notice contained in this website, as well as with the rules of coexistence, morality and generally accepted good customs. The User is obliged and undertakes not to transmit, disseminate or make available to third parties any content of the website, such as information, text, data, content, messages, graphics, drawings, sound and/or image files, photographs, recordings, software, logos, brands, icons, technology, photographs, software, links, graphic design and source codes, or any other material to which he has access in his capacity as a User of the website, without this list being limitative.</p>
+                {/*<p>The user, once registered as such on the website/online shop, which is essential for acquiring the products offered through them, has a personal area, identified as “My Account”, in which he can access the link “View or modify your data” and thus identify and rectify any errors made in entering data.</p>*/}
+                {/*<p>When a User places an order, he/she will receive an e-mail with the web order form, which lists all the products he/she wishes to purchase, indicating the purchase prices and a link from which he/she can consult these contract conditions. Once the order request has been made, it will be processed and the User will receive a second e-mail with the information about the person in charge of the order. If any situation related to the stock arises, variation of delivery time, etc…, the Offeror will contact the User via telephone or email in its absence to inform him/her at all times.</p> */}
+                {/*<p>The order confirmation may be printed by the User as a proof of purchase, but it will not be valid as an invoice under any circumstances.</p> */}
+                {/*<p>The order will be accompanied by the invoice at the time of delivery to the address. In the section “My Orders”, located on the home page in the section “My Account” the User will also find a history of all orders placed with Offeror. By clicking on the order number, the User will find the list of products purchased, the price and the method of payment.</p> */}
+
+                <h3>3.- INTELLECTUAL PROPERTY</h3>
+                <p>All trademarks, trade names or logos of any kind that appear on the Website are the property of the Offeror or, where appropriate, of third parties who have authorized their use, and it may not be construed that use of or access to the Website and/or its contents gives the User any right over such trademarks, trade names and/or logos, and none of the exploitation rights that exist or may exist over such contents may be construed as having been assigned to the User. And it does not understand assigned nor its use is authorized, in any case, to the Users of the website unless these rights are obtained in writing from the legitimate owner of the same.</p>
+            
+                <h3>4.- PRODUCT OFFER AND PRICE REFERENCE</h3>
+                <p>In compliance with current regulations, the Offeror offers in each of the sections, according to the type of product in question, information on all the articles for sale, descriptions, prices and approximate delivery time of the same. However, the Offeror reserves the right to withdraw, replace or change the products offered to its customers through its website/online shop, by simply changing the content of the website. In this way, the products offered at all times will be governed by the GTC in force in each case. Likewise, the company will have the right to stop offering, without previous notice and at any time, the access to the mentioned products.</p>
+            
+                <h4>5.- INDICATION OF PRICES</h4>
+                <p>The prices indicated on the screen are shown in euros and include VAT and any other tax that may be applicable, and will be the ones in force at all times, except in the case of a typographical error.</p>
+                
+                <h4>6.- SHIPPING COSTS AND TYPES OF DELIVERY</h4>
+                <p>The shipping costs and type of delivery is the subject of written quotation of the Offeror which is not defined in the GTC.</p>
+
+                <h4>7.- FORMS OF PAYMENT</h4>
+                <p>The ordered products has to be prepaid before delivery according to the invoice issued by the Offeror.</p>
+
+                
             </div>
 
 
