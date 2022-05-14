74ec940757149deedd556edd0b7e7a45649b5ef7
bszeidl <szeidl.balazs@hotmail.com>
2022-05-14 22:51:58 +0200

    Modal button and modal window ready with production license text, animated, working properly.

--- frontend/src/components/structure_components/4_Hero.jsx
+++ frontend/src/components/structure_components/4_Hero.jsx
@@ -1,12 +1,49 @@
-import React from 'react';
+import {React, useState} from 'react';
 
 const Hero = () => {
+
+    const [modal, setModal] = useState(false);
+
+    
+    /*
+    const forSaleSwitch = () => {
+        alert('Production license is for sale including full documentation for manufacturing. The product is patent protected. Patent registration number: 005188. For more information call or write the contact!')
+    }
+    */
+
+    const forSaleSwitch = (e) => {
+        setModal(!modal);
+    }
+
+
 
     return (
         
         <div className="intro-container">
-            <div className="hero-image" ></div>
-            <div className="forsale"><div>Production license for sale!</div></div>
+
+            <div className="forsale-wrap">
+                <div className="disc" onClick={forSaleSwitch}></div>
+                <button className="forsale" >Production license for sale!</button>
+            </div>
+
+            <div className="hero-image" >            
+        
+            {
+
+                modal === true 
+
+                ?
+
+                <div className="modal">
+                    <div className="modal-text" onClick={forSaleSwitch}><a className="modal-text" href="#contact-id">Production license is for sale including full documentation for manufacturing. The product is patent protected. Patent registration number: 005188. For more information call or write the contact!</a></div>
+                </div>
+
+                :
+
+                null
+            }
+        
+            </div>
         </div>
 
     );
