faa3b83193721a15bf44c3d95ed54c03da70e15d
bszeidl <szeidl.balazs@hotmail.com>
2022-06-07 08:43:43 +0200

    Nested list in Benefits section improed.

--- frontend/src/components/structure_components/5_0_Benefits_eng.jsx
+++ frontend/src/components/structure_components/5_0_Benefits_eng.jsx
@@ -11,36 +11,36 @@ const BenefitsEng = () => {
             <h2>INTRODUCTION</h2>
 
             <ul className="benefits-list">
-                <li>BALOREA WEB is the web site development service of BALOREA TEAM Ltd. 
-                    <ul className="nested-list">
+                <p>BALOREA WEB is the web site development service of BALOREA TEAM Ltd. 
+                    <ul>
 
-                        <li>Web developer: Balázs Szeidl</li>
-                        <li>Background: marketing and sales management, web site development:
+                        <p>Web developer: Balázs Szeidl</p>
+                        <p>Background: marketing and sales management, web site development:
                             <ul>
-                                <li>I worked as country and international marketing manager of various multinational companies in agricultural business.</li>
-                                <li>I have been responsible for business (annual budget and long range planning) and marketing campaign planning in ten countries.</li>
-                                <li>I have been participated in international brand development campaign and I have been responsible for the implementation of new branding guidelines in ten countries.</li>
-                                <li>As national sales manager I have been responsible for: 
+                                <p>I worked as country and international marketing manager of various multinational companies in agricultural business.</p>
+                                <p>I have been responsible for business (annual budget and long range planning) and marketing campaign planning in ten countries.</p>
+                                <p>I have been participated in international brand development campaigns and I have been responsible for the implementation of new branding guidelines in ten countries.</p>
+                                <p>As national sales manager I have been responsible for: 
                                     <ul>
-                                        <li>key performance indicator (KPI) planning and monitoring,</li>
-                                        <li>customer relationship management (CRM) system,</li>
-                                        <li>sales representative coaching and mentoring.</li>
+                                        <p>key performance indicator (KPI) planning and monitoring,</p>
+                                        <p>customer relationship management (CRM) system,</p>
+                                        <p>sales representative coaching and mentoring.</p>
                                     </ul>
-                                </li>
+                                </p>
                             </ul>
-                        </li>
-                        <li>As web developer I use HTML, CSS, JavaScript with React.</li>
-                        <li>I can help my clients combining my sales and marketing managment insights with web site and web site design development, furthermore new branding elements such as corporate or product logos, tags etc.</li>
+                        </p>
+                        <p>As web developer I use HTML, CSS, JavaScript with React.</p>
+                        <p>I can help my clients combining my sales and marketing managment insights with web site and web site design development, furthermore new branding elements such as corporate or product logos, tags etc.</p>
                         
                     </ul>
-                </li>
-                <li>Benefits 
+                </p>
+                <p>Benefits 
                     <ul>
-                      <li>Synergy of marketing & sales management experience with the combination of web site development.</li>
-                      <li>User friendly, simple, easy to use, ergonomic site handling.</li>
-                      <li>Fast page load, lightning quick switchning between menus and sections.</li>
+                      <p>Synergy of marketing & sales management experience with the combination of web site development.</p>
+                      <p>User friendly, simple, easy to use, ergonomic site handling.</p>
+                      <p>Fast page load, lightning quick switchning between menus and sections.</p>
                     </ul>
-                </li>               
+                </p>               
             </ul>
 
         </div>            
