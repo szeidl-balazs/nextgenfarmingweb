29086e4d696d46a16865fabff3965d2e5fd6c4ee
bszeidl <szeidl.balazs@hotmail.com>
2022-06-07 17:15:24 +0200

    Introduction and benefits ready including media queries.

--- frontend/src/components/structure_components/5_0_Benefits_eng.jsx
+++ frontend/src/components/structure_components/5_0_Benefits_eng.jsx
@@ -10,38 +10,19 @@ const BenefitsEng = () => {
         
             <h2>INTRODUCTION</h2>
 
-            <ul className="benefits-list">
-                <p>BALOREA WEB is the web site development service of BALOREA TEAM Ltd. 
-                    <ul>
-
-                        <p>Web developer: Balázs Szeidl</p>
-                        <p>Background: marketing and sales management, web site development:
-                            <ul>
-                                <p>I worked as country and international marketing manager of various multinational companies in agricultural business.</p>
-                                <p>I have been responsible for business (annual budget and long range planning) and marketing campaign planning in ten countries.</p>
-                                <p>I have been participated in international brand development campaigns and I have been responsible for the implementation of new branding guidelines in ten countries.</p>
-                                <p>As national sales manager I have been responsible for: 
-                                    <ul>
-                                        <p>key performance indicator (KPI) planning and monitoring,</p>
-                                        <p>customer relationship management (CRM) system,</p>
-                                        <p>sales representative coaching and mentoring.</p>
-                                    </ul>
-                                </p>
-                            </ul>
-                        </p>
-                        <p>As web developer I use HTML, CSS, JavaScript with React.</p>
-                        <p>I can help my clients combining my sales and marketing managment insights with web site and web site design development, furthermore new branding elements such as corporate or product logos, tags etc.</p>
-                        
-                    </ul>
-                </p>
-                <p>Benefits 
-                    <ul>
-                      <p>Synergy of marketing & sales management experience with the combination of web site development.</p>
-                      <p>User friendly, simple, easy to use, ergonomic site handling.</p>
-                      <p>Fast page load, lightning quick switchning between menus and sections.</p>
-                    </ul>
-                </p>               
-            </ul>
+            <div className="benefits-list">
+              <p>BALOREA WEB is the web site development service of BALOREA TEAM Ltd.</p>          
+              <p>Web developer: Balázs Szeidl</p>
+              <p>Background: marketing and sales management in international business environment, web site development. I have experience in developing and leading the implementation of sales and marketing campaigns, I have participated in multinational branding projects.</p>
+              <p>As web developer I use HTML, CSS, JavaScript with React.</p>
+              <p>I can help my clients combining my sales and marketing managment insights with web site and web site design development, furthermore new branding elements such as corporate or product logos, tags etc.</p>
+              <p>Benefits</p> 
+                <ul>
+                  <p>Synergy of marketing & sales management experience with the combination of web site development.</p>
+                  <p>User friendly, simple, easy to use, ergonomic site handling.</p>
+                  <p>Fast page load, lightning quick switchning between menus and sections.</p>
+                </ul>
+            </div>
 
         </div>            
 
