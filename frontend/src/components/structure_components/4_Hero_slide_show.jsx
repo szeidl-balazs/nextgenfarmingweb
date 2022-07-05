a12e6b3705b0a273deaaca063250530c7e9c1484
bszeidl <szeidl.balazs@hotmail.com>
2022-07-05 22:59:42 +0200

    Carousel image appeared with css background image technique. To optimize size setting with core ui paramters.

--- frontend/src/components/structure_components/4_Hero_slide_show.jsx
+++ frontend/src/components/structure_components/4_Hero_slide_show.jsx
@@ -14,7 +14,7 @@ const SlideShow = () => {
         <CCarousel controls indicators className="carousel-container">
 
             <CCarouselItem>
-                <CImage  className="d-block w-100" fluid src="../images/weddingzoeandalex.png" alt="slide 1" />
+                <CImage  className="d-block w-100" fluid id="carousel1" alt="slide 1" />
                 <CCarouselCaption className="d-none d-md-block">
                     <h5>First slide label</h5>
                     <p>Some representative placeholder content for the first slide.</p>
