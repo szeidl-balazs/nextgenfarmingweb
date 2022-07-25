b50c7c411c0950822384be278903276fbf1450d7
bszeidl <szeidl.balazs@hotmail.com>
2022-07-25 15:01:06 +0200

    The introduction and services sections have been strucutred into columns with core ui CCol technology, now the width of text is more reader friendly and perfectly responsive. Hungarian version tbd.

--- frontend/src/components/structure_components/5_0_Benefits_eng.jsx
+++ frontend/src/components/structure_components/5_0_Benefits_eng.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { CContainer } from '@coreui/react'
+import { CCol } from '@coreui/react'
+import { CRow } from '@coreui/react'
 
 const BenefitsEng = () => {
 
@@ -11,20 +14,34 @@ const BenefitsEng = () => {
         
             <h2>INTRODUCTION</h2>
 
-            <div className="benefits-list">
-              <p>BALOREA WEB is the frontend web site development service of BALOREA TEAM Ltd.</p>          
-              <p>Web developer: Balázs Szeidl</p>
-              <p>Background: marketing and sales management in international business environment, web site development. I have experience in developing and leading the implementation of sales and marketing campaigns, I have participated in multinational branding projects.</p>
-              <p>As web developer I use HTML, CSS, JavaScript with React.</p>
-              <p>I can help my clients combining my sales and marketing managment insights with web site and web site design development, furthermore new branding elements such as corporate or product logos, tags etc.</p>
-              <p>Benefits</p> 
+            <CContainer fluid className="benefits-list">
+
+             <CRow xs={{cols: 1}} sm={{cols: 2, gutter: 5}} xxl={{cols: 3}}>
+
+              <CCol >
+                <p>BALOREA WEB is the frontend web site development service of BALOREA TEAM Ltd.</p>          
+                <p>Web developer: Balázs Szeidl</p>
+                <p>Background: marketing and sales management in international business environment, web site development. I have experience in developing and leading the implementation of sales and marketing campaigns, I have participated in multinational branding projects.</p>
+              </CCol>
+
+              <CCol>
+                <p>As web developer I use HTML, CSS, JavaScript with React.</p>
+                <p>I can help my clients combining my sales and marketing managment insights with web site and web site design development, furthermore new branding elements such as corporate or product logos, tags etc.</p>
+              </CCol>
+
+              <CCol>
+                <p>Benefits</p> 
                 <ul>
                   <p>Synergy of marketing & sales management experience with the combination of web site development.</p>
                   <p>User friendly, simple, easy to use, ergonomic site handling.</p>
                   <p>Responsive sizing, readable, recognisable, handleable at any screen size.</p>
                   <p>Fast page load, lightning quick switchning between menus and sections.</p>
                 </ul>
-            </div>
+              </CCol>      
+
+             </CRow>
+
+            </CContainer>
 
         </div>            
 
--- frontend/src/components/structure_components/6_Presentations_eng.jsx
+++ frontend/src/components/structure_components/6_Presentations_eng.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { CContainer } from '@coreui/react'
+import { CCol } from '@coreui/react'
+import { CRow } from '@coreui/react'
 
 const PresentationsEng = () => {
 
@@ -9,20 +12,37 @@ const PresentationsEng = () => {
             
             <h2>SERVICES</h2>
 
+            <CContainer fluid>
+
+            <CRow xs={{cols: 1}} sm={{cols: 2, gutterX: 5}} xxl={{cols: 4}}>
+
+            <CCol>
             <h3>WEB SITE AUDIT</h3>
             <p>Auditing current web site focusing on user interface (UI) and user experience (UX).</p>
             <p>Developing action plan to improve the user interface and user experience of current web site.</p>
+            </CCol>
+
+            <CCol>
             <h3>WEB SITE DESIGN</h3>
             <p>Surveying client's need regarding new web site.</p>
             <p>Developing new web site designs and drafting proposals.</p>
+            </CCol>
+
+            <CCol>
             <h3>WEB SITE PROGRAMMING</h3>
             <p>Surveying client's need regarding new web site.</p>
             <p>Programming new web sites.</p>
             <p>Focus service is the customised programming of landing pages according to clients' individual needs.</p>
+            </CCol>
+
+            <CCol>
             <h3>BRANDING ELEMENT DESIGN</h3>
             <p>Surveying client's need regarding branding elements' need such as logos, tags etc.</p>
             <p>Developing the design of branding elements.</p>
+            </CCol>
 
+            </CRow>
+            </CContainer>
         </div>
 
     );
