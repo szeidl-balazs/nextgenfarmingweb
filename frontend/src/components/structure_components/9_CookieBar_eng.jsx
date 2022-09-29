e5f92181349773bcf60702a300b31a430d82767e
bszeidl <szeidl.balazs@hotmail.com>
2022-09-29 16:28:41 +0200

    Cookie bar - slideshow conflict improved, but still the cookie bar too high.

--- frontend/src/components/structure_components/9_CookieBar_eng.jsx
+++ frontend/src/components/structure_components/9_CookieBar_eng.jsx
@@ -22,9 +22,13 @@ const CookieBarEng = (Cookies) => {
                 <div className="cookie-bar">
                     <div className="message"> 
                       <p>Dear Visitor! In order to provide you the best user experience we use "cookies".</p>
-                      <p>Using this website you accept our <a href="Cookies" target="_blank" rel="noreferrer">cookie policy</a>.</p>
+                      <p>Using this website you accept our 
+                        <a href="Cookies" target="_blank" rel="noreferrer">cookie policy</a>.
+                      </p>
                     </div>
-                    <div className="mobile">This website uses cookies, <a href="7_2_cookie_text.html" target="_blank">details</a>.</div>
+                    <div className="mobile">This website uses cookies, 
+                      <a href="7_2_cookie_text.html" target="_blank">details</a>.
+                    </div>
                     <button className="accept-btn close-cb" onClick={cookiebarCloser}>I ACCEPT.</button>
                 </div>   
 
