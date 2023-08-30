d9a2d1f3790d45e9abe00a3289bd2e73573e2c2b
bszeidl <szeidl.balazs@hotmail.com>
2023-08-30 23:10:38 +0200

    Dark and light color set to brand grphic code. Carousel photos settled.

--- frontend/src/components/structure_components/4_Hero_slide_show.jsx
+++ frontend/src/components/structure_components/4_Hero_slide_show.jsx
@@ -3,10 +3,11 @@ import { CCarousel } from '@coreui/react';
 import { CCarouselItem } from '@coreui/react';
 import { CImage } from '@coreui/react';
 import '@coreui/coreui/dist/css/coreui.min.css';
-import wedding from '../images/weddingzoeandalex.png'
-import pizza from '../images/pizza.png';
-import gocsej from '../images/gocsej.png';
-import farkasgym from '../images/farkasgym1.png';
+import wosr_noon from '../images/wosr_noon.jpg';
+import wheat_sunset from '../images/wheat_sunset.jpg';
+import corn_side from '../images/corn_side.jpg';
+import soy_sunset from '../images/soy_sunset.jpg';
+import sunflower_close from '../images/sunflower_close.jpg';
 
 
 const SlideShow = () => {   
@@ -17,19 +18,23 @@ const SlideShow = () => {
         <CCarousel controls indicators className="carousel-container">
 
             <CCarouselItem>
-                <CImage className="d-block w-100" src={wedding} alt="slide 3" />
+                <CImage className="d-block w-100" src={wosr_noon} alt="slide 1" />
             </CCarouselItem>
 
             <CCarouselItem>
-                <CImage className="d-block w-100 slide-image" fluid src={gocsej} alt="slide 1"/>     
+                <CImage className="d-block w-100 slide-image" fluid src={corn_side} alt="slide 2"/>     
             </CCarouselItem>
 
             <CCarouselItem>
-                <CImage className="d-block w-100" src={pizza} alt="slide 2" />
+                <CImage className="d-block w-100" src={wheat_sunset} alt="slide 3" />
             </CCarouselItem>
 
             <CCarouselItem>
-                <CImage className="d-block w-100" src={farkasgym} alt="slide 3" />
+                <CImage className="d-block w-100" src={sunflower_close} alt="slide 4" />
+            </CCarouselItem>
+
+            <CCarouselItem>
+                <CImage className="d-block w-100" src={soy_sunset} alt="slide 5" />
             </CCarouselItem>
 
         </CCarousel> 
